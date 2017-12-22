c5dfe5c7de385808df4a93cad52c9277a52a033b
metamorph-online <metamorph_online@yahoo.com>
2017-12-23 00:31:27 +0200

    fixing react-router urls

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
+app.get('*', function (req, res) { // This wildcard method handles all requests
+
+    Router.run(routes, req.path, function (Handler, state) {
+        var element = React.createElement(Handler);
+        var html = React.renderToString(element);
+        res.render('main', { content: html });
+    });
+});
