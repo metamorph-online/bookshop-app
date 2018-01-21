8b33a0cf9fc68808b30407486314a0d3b521712c
metamorph-online <metamorph_online@yahoo.com>
2018-01-21 03:51:45 +0200

    google auth continue

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
-var app = express();
+const app = express();
+const path = require('path');
 var passport = require('passport');
 const authRouts = require('./routes/authRoutes');
 
 authRouts(app);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
